2c83512f9ce9a462dff9089058c82f5b8978dd2e
cct1225 <grace951225@gmail.com>
2024-01-20 22:21:40 +0800

    20240120

---
+++ zerojudge/b231.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n,i=0;
+    int a[100],b[100];
+    int max=0;
+    while(cin>>n)
+    {
+        cin>>a[i]>>b[i];
+        i++;
+        
+    }
+}
